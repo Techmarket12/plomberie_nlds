dd16c061efeba0f3301573c6b4842c7dd929fd29
Techmarket12 <96696189+Techmarket12@users.noreply.github.com>
2025-07-16 19:45:12 +0200

    Update index.tsx

--- app/zones-intervention/index.tsx
+++ app/zones-intervention/index.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation"
 
 export default function ZonesInterventionIndex() {
+  // La redirection reste la même, car il s'agit d'un chemin interne
   redirect("/zones-intervention")
 }
