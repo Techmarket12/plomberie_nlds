553491c0b837a2fa6e8e7641b09a1f2f18fbee1c
Techmarket12 <96696189+Techmarket12@users.noreply.github.com>
2025-07-16 19:41:08 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,10 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://debouchage-plomberie-service24.be/sitemap.xml",
+    // Si vous avez un sitemap spécifique pour le néerlandais, changez l'URL ici.
+    // Par exemple, si c'est sur un sous-domaine comme nl.aquaetdeb.be/sitemap.xml
+    // Ou si votre sitemap gère l'i18n, l'URL pourrait rester la même mais son contenu varier.
+    // Pour l'instant, je le mets sur le domaine principal que nous avons utilisé précédemment.
+    sitemap: "https://aquaetdeb.be/sitemap.xml",
   }
 }
